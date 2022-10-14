d899a7c191e1d7a79f136ea819f4635f1f729256
10Strel <manowartenarrows@gmail.com>
2022-10-14 21:46:21 +0300

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿const int limit = 3;
+
+string source = 
+    "Я попугай с антильских островов\r\n" + 
+    "Но я живу в квадратной келье мага\r\n" +
+    "Вокруг реторты глобусы бумага\r\n" +
+    "И кашель старика и бой часов";
+
+string[] sourceArray =  source.Replace("\r\n", " ").Split(new char[] {' '});
+string[] resultArray = Processing(sourceArray);
+
+PrintResults();
+
+# region methods
+
+string[] Processing(string[] array)
+{    
+    string[] result = new string[0];
+    
+    foreach (string elem in array)
+    {
+        Console.WriteLine(elem.Length);
+
+        if (elem.Length <= limit)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[result.Length-1] = elem;            
+        }
+    }
+
+    return result;
+}
+
+void PrintResults()
+{ 
+    string resultStr = "[";
+
+    foreach (string str in sourceArray)
+    {
+        resultStr = string.Concat(resultStr, "\"", str, "\",");    
+    }
+    
+    resultStr = string.Concat(resultStr, "]");
+
+    if (resultArray.Length > 0)
+    {
+        resultStr = string.Concat(resultStr, " -> ");
+
+        foreach (string str in resultArray)
+        {
+            resultStr = string.Concat(resultStr, "\"", str, "\",");
+        }
+
+        resultStr = resultStr.Substring(0, resultStr.Length-1);
+    }
+    
+    Console.WriteLine(resultStr);
+}
+
+#endregion
